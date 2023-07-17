34678dcf11dff0763540d3f9011a936e3a27d430
HanHyunsoo <gustn4563@gmail.com>
2023-07-17 15:44:53 +0900

    feat: Add hello1 api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,11 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('/hello1')
+  getHello1(): string {
+    return 'Hello World!1';
+  }
+
   @Get('/hello2')
   getHello(): string {
     return this.appService.getHello();
